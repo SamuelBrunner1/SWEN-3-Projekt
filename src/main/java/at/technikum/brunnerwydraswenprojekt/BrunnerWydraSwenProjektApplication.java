111b783b1792cd8486a6160d04d65ad07eb157e5
Samuel Brunner <samuel.brunner16@gmail.com>
2025-09-11 13:49:46 +0200

    project setup inkl REST Server

---
+++ src/main/java/at/technikum/brunnerwydraswenprojekt/BrunnerWydraSwenProjektApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.brunnerwydraswenprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunnerWydraSwenProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrunnerWydraSwenProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/brunnerwydraswenprojekt/BrunnerWydraSwenProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.brunnerwydraswenprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunnerWydraSwenProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
