f1507f701ec37cf77c2f71f628bbe809e84de911
tobia <tobias.wydra@outlook.com>
2025-10-09 16:16:40 +0200

    Added RabbitMQWorker base structure

---
+++ rabbitmq-worker/src/main/java/at/technikum/swen_brunner_wydra/rabbitmqworker/RabbitmqWorkerApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.swen_brunner_wydra.rabbitmqworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqWorkerApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-worker/src/test/java/at/technikum/swen_brunner_wydra/rabbitmqworker/RabbitmqWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.swen_brunner_wydra.rabbitmqworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
