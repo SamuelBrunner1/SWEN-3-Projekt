8342e25968836ec1be35f2cee1bb7b160d636143
Samuel Brunner <samuel.brunner16@gmail.com>
2025-10-12 00:23:09 +0200

    rabbitmq setup

--- src/main/java/at/technikum/swen_brunner_wydra/service/DocumentService.java
+++ src/main/java/at/technikum/swen_brunner_wydra/service/DocumentService.java
@@ -1,18 +1,23 @@
 package at.technikum.swen_brunner_wydra.service;
 
+import at.technikum.swen_brunner_wydra.config.RabbitConfig;
 import at.technikum.swen_brunner_wydra.entity.Dokument;
 import at.technikum.swen_brunner_wydra.repository.DokumentRepository;
 import at.technikum.swen_brunner_wydra.service.dto.DocumentDTO;
 import at.technikum.swen_brunner_wydra.service.mapper.DocumentMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class DocumentService {
 
     private final DokumentRepository repository;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate; // <--- RabbitMQ Template
 
     public DocumentService(DokumentRepository repository) {
         this.repository = repository;
@@ -33,6 +38,10 @@ public class DocumentService {
     public DocumentDTO save(DocumentDTO dto) {
         Dokument entity = DocumentMapper.toEntity(dto);
         Dokument saved = repository.save(entity);
+
+        // Nachricht an RabbitMQ senden:
+        rabbitTemplate.convertAndSend(RabbitConfig.QUEUE_NAME, saved.getId());
+
         return DocumentMapper.toDto(saved);
     }
 
