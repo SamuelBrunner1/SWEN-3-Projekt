6127e29aab7897b7603cdafe55c2bf2e495ffe78
Samuel Brunner <samuel.brunner16@gmail.com>
2026-01-19 17:02:03 +0100

    Add access log batch processing setup

---
+++ log-batch/src/main/java/at/technikum/logbatch/LogBatchApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.logbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogBatchApplication.class, args);
+    }
+
+}
---
+++ log-batch/src/test/java/at/technikum/logbatch/LogBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.logbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
