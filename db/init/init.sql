62ea1a3c30b4450748a1943b0b2bfc12d3b8a780
Samuel Brunner <samuel.brunner16@gmail.com>
2026-01-20 20:18:08 +0100

    Add log-batch worker with scheduled XML batch processing

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,3 +1,7 @@
-CREATE USER postgres WITH PASSWORD 'postgres';
-CREATE DATABASE swenprojekt OWNER postgres;
-GRANT ALL PRIVILEGES ON DATABASE swenprojekt TO postgres;
+CREATE TABLE IF NOT EXISTS document_access_stats (
+                                                     id BIGSERIAL PRIMARY KEY,
+                                                     document_id BIGINT NOT NULL,
+                                                     access_date DATE NOT NULL,
+                                                     access_count INT NOT NULL,
+                                                     CONSTRAINT uq_document_access UNIQUE (document_id, access_date)
+    );
